e757cece2511e4e17d69cb5f883e3469fc029ccc
Wei Wei <weiw4180@gmail.com>
2018-10-23 00:52:49 -0400

    Add some new functions

--- BookStore0-WeiWei/src/com/team404/bookstore/service/OrderProcessService.java
+++ BookStore0-WeiWei/src/com/team404/bookstore/service/OrderProcessService.java
@@ -1,14 +1,19 @@
 package com.team404.bookstore.service;
 
 import com.team404.bookstore.dao.AddressDao;
+import com.team404.bookstore.dao.ShoppingCartDao;
 import com.team404.bookstore.dao.UserDao;
 import com.team404.bookstore.entity.AddressEntity;
+import com.team404.bookstore.entity.ShoppingCartEntity;
 import com.team404.bookstore.entity.UserEntity;
 import org.hibernate.internal.build.AllowSysOut;
+
+import java.util.List;
 
 public class OrderProcessService {
     private UserDao userDao;
     private AddressDao addressDao;
+    private ShoppingCartDao shoppingCartDao;
 
     /*Submit Function*/
     public boolean CreateAccount(UserEntity userEntity, AddressEntity addressEntity) {
@@ -49,6 +54,14 @@ public class OrderProcessService {
         return flag;
     }
 
+    public UserEntity GetUserByAccount(String username) {
+        userDao = new UserDao();
+
+        UserEntity userEntity = userDao.GetUserbyAccount(username);
+
+        return  userEntity;
+    }
+
     /*Personal Info-UserAccount Info*/
     public UserEntity getUserinfo(int id) {
         userDao = new UserDao();
@@ -66,5 +79,28 @@ public class OrderProcessService {
 
         return addressEntity;
     }
+
+    /*Customer add item(s) to their own shopping cart*/
+    public boolean AddItemtoCart(ShoppingCartEntity shoppingCartEntity) {
+
+        shoppingCartDao = new ShoppingCartDao();
+
+        return shoppingCartDao.AddShoppingCart(shoppingCartEntity);
+    }
+
+    /*Display customers' shopping cart contents*/
+    public List<ShoppingCartEntity> DisplayShoppingCart(int userid) {
+        shoppingCartDao = new ShoppingCartDao();
+
+        return shoppingCartDao.GetShoppingCartByUid(userid);
+    }
+
+    /*Delete single item in shopping cart*/
+    public boolean DeleteSingleItem(int id) {
+        shoppingCartDao = new ShoppingCartDao();
+
+        return shoppingCartDao.DeleteShoppingItemById(id);
+    }
+
 
 }
